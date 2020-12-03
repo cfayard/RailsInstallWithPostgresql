a0e03c08a2a979abe7a931f8dae93357c13fd277
Casey Fayard <fayardcasey@gmail.com>
2020-12-03 11:49:04 -0500

    writing condtionals within controllers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def home
-    @message = "Welcome to my homepage!"
+    some_value = false
+
+    if some_value === true
+      @message = "Welcome to my homepage!"
+    else
+      @message = "Hope you are having a great day!"
+    end
   end
 
   def features
